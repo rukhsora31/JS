4f66edf5f95303752928b19ee39df3c891ce92de
rukhsora31 <ruhsora447@gmail.com>
2024-08-13 16:56:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+// let a = 23
+
+// alert(typeof a);
+
+// if (5 < 4) {
+//     console.log('true');
+// } else {
+//     console.log('false');
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// let name = prompt('What is your name?')
+// let age =  +prompt('how old are you?')
+// let city = prompt('where do you live?')
+
+// alert('Hello '  + name + ' тебе ' + age + ' лет,' + ' ты живешь в ' + city  )
+// alert(`привет ${name}, вам ${age} лет, вы живете в городе ${city}`)
+
+// const sayHello = (name) => {
+//     alert(`hello ${name}`)
+// }
+// sayHello('rhs')
+
+// let user = {
+//     name: 'Ruxsora',
+//     surname: 'Axmadjonova'
+// }
+
+// user.age = 16
+// delete user.age
+
+// console.log(user);
+
+
+// let num1 = +prompt('enter the first number')
+// let num2 = +prompt('enter the second number')
+
+// if (num1 > num2) {
+//     alert('pervoe chislo bolshe vtorogo')
+// } else if(num1 < num2) {
+//     alert('vtoroye chislo bolshe pervogo')
+// }else{
+//     alert('oni ravni')
+// }
+
+
+// let number = +prompt('Введите число')
+// if (number == 0) {
+//     alert('число равен нулю')
+// } else if (number % 2 == 0) {
+//     alert('число чётное')
+// } else{
+//     alert('число нечётное')
+// }
+
+
+// let secretNumber = Math.floor(Math.random() *11)
+// let guess = +prompt('угадай число от 1 до 10')
+
+// if (guess == secretNumber) {
+//     alert('ты угадал! Поздравляю 🎊')
+// } else {
+//     alert('не угадал! Попробуй ещё раз( ')
+// }
+
+// console.log(secretNumber);
